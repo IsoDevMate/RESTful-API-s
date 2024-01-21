69e664a14054ecb42db343e5628872a92c40467d
IsoDevMate <oumaoduor5827@gmail.com>
2024-01-21 16:16:00 +0300

    working ec2

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express =require('express')
 const app =express()
 const  port= process.env.PORT || 8080
-app.listen(
-    port,
-    () =>console.log(`It's alive on http://localhost:${port}`)
-)
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.get('/',(req,res) => {
+  res.send('Hey there EC2 INstance running')
+})
 
 app.get('/tshirt',(req,res) =>{
   res.status(200).send({
@@ -26,5 +34,16 @@ app.post('/tshirt/:id',(req,res) =>{
             tshirt:`👕 with your ${logo} and ID of ${id}`,
         })
         })
+
+        
+app.use((req, res) => {
+  res.status(404).send({message: 'Not found'});
+});
+
+
+ app.listen(
+          port,
+          () =>console.log(`It's alive on http://localhost:${port}`)
+      )        
 
 
